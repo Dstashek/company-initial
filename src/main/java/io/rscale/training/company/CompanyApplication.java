4fb5ff16ad8f16313bea97f662ea0ffb386e8853
AMS\daniel.stashek <daniel.stashek@cgi.com>
2017-06-07 12:06:10 -0600

    added eureka annotation

--- src/main/java/io/rscale/training/company/CompanyApplication.java
+++ src/main/java/io/rscale/training/company/CompanyApplication.java
@@ -3,7 +3,10 @@ package io.rscale.training.company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @EnableDiscoveryClient
 @SpringBootApplication
 public class CompanyApplication {
